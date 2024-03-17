5c41d6e490eb01ac31fb3ed9905bd9cd5a93e957
jeffmorales29 <jeffmorales29@yahoo.com>
2024-03-18 00:10:56 +0800

    First Commit

---
+++ timezone.py
@@ -0,0 +1,57 @@
+# Project : Timezone Database
+# Author : Jeffrey Morales
+#Email: jeffmorales29@gmail.com
+
+
+import sqlite3
+import requests
+
+# Connect to SQLite database
+conn = sqlite3.connect('TimezoneDB.db')
+cursor = conn.cursor()
+
+# Create a table if not exists
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS TZDB_TIMEZONES (
+        COUNTRYCODE VARCHAR2 NOT NULL,
+        COUNTRYNAME VARCHAR2 PRIMARY KEY NOT NULL,
+        ZONENAME VARCHAR2 NOT NULL,
+        GMTOFFSET INTEGER,
+        IMPORT_DATE DATE
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS TZDB_ZONE_DETAILS (
+        COUNTRYCODE VARCHAR2 NOT NULL,
+        COUNTRYNAME VARCHAR2 NOT NULL,
+        ZONENAME VARCHAR2 NOT NULL,
+        GMTOFFSET INTEGER,
+        IMPORT_DATE DATE
+    )
+''')
+
+
+#API data
+url = "http://api.timezonedb.com/v2.1/list-time-zone"
+api_key = "VFUCYVC71XME"
+
+location = "London"
+
+params = {
+    "key": api_key,
+    "format": "json",  # Response format
+    "by": "zone",      # Search by time zone
+    "zone": location  # Location parameter
+}
+
+# Make GET request
+response = requests.get(url, params=params)
+
+# Check response status code
+if response.status_code == 200:
+    # Print response content
+    print(response.json())
+else:
+    # Print error message
+    print("Error:", response.status_code)
